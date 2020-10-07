b68b28d0a4b16327312dbe394aa70928c3e5ab1d
realspectr3x <realspectr3x@vort3xmedia.cf>
2020-10-06 20:41:52 -0400

    modid

---
+++ src/main/java/me/github/thirtyFirst/ThirtyFirst.java
@@ -0,0 +1,10 @@
+package me.github.thirtyFirst;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ThirtyFirst implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
